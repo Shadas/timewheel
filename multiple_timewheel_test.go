be31857d2f0468e7e900294f862f8f7bf873d3f8
Shadas <shadas.zhao@gmail.com>
2021-08-06 15:36:33 +0800

    Add test case

--- multiple_timewheel_test.go
+++ multiple_timewheel_test.go
@@ -32,6 +32,12 @@ func TestPosAddOffset(t *testing.T) {
 	if pos := posAddOffset([]int{200, 3, 5}, 100); !isPosEqual(pos, []int{44, 4, 5}) {
 		t.Errorf("pos is not [44,4,5], it's %v", pos)
 	}
+	if pos := posAddOffset([]int{200, 3, 5}, 30); !isPosEqual(pos, []int{230, 3, 5}) {
+		t.Errorf("pos is not [230,3,5], it's %v", pos)
+	}
+	if pos := posAddOffset([]int{200, 63, 5}, 1024); !isPosEqual(pos, []int{200, 3, 6}) {
+		t.Errorf("pos is not [200,3,6], it's %v", pos)
+	}
 }
 
 func TestAppendPos(t *testing.T) {
